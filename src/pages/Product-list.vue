bf4ad996f799e0f8dad920ba4f29d16e8d257336
Stew <ghost.pizza@gmail.com>
2021-02-05 21:51:37 +0100

    Update Product-list.vue

--- src/pages/Product-list.vue
+++ src/pages/Product-list.vue
@@ -38,8 +38,8 @@
       <div class="col-lg-3 col-md-12 my-sm-auto mt-4">
         <p class="price-text text-center mb-1" v-if="product.price != '0,0'">{{ product.price }}£</p>
         <p class="price-text text-center mb-1" v-else>Offer</p>
-        <button type="button" class="btn btn-danger btn-block" v-if="product.price != '0,0'">Buy Now</button>
-         <button type="button" class="btn btn-danger btn-block" v-else>Check availability</button>
+   <a :href="product.uRL + affiliate" type="button" class="btn btn-danger btn-block" v-if="product.price != '0,0'">Buy Now</a>
+         <a :href="product.uRL + affiliate" type="button" class="btn btn-danger btn-block" v-else>Check availability</a>>
       </div>
     </div>
   </div>
